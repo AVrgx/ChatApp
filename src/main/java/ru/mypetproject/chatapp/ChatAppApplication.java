68422bb600afc17b6e79aba9416be9224809d855
AvargX <avargx@gmail.com>
2025-08-06 16:58:44 +0900

    feat: update ChatAppApplication

--- src/main/java/ru/mypetproject/chatapp/ChatAppApplication.java
+++ src/main/java/ru/mypetproject/chatapp/ChatAppApplication.java
@@ -1,13 +1,33 @@
 package ru.mypetproject.chatapp;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import ru.mypetproject.chatapp.model.ChatRoom;
+import ru.mypetproject.chatapp.repository.ChatRoomRepository;
 
 @SpringBootApplication
 public class ChatAppApplication {
 
+    @Autowired
+    private ChatRoomRepository chatRoomRepository;
+
     public static void main(String[] args) {
         SpringApplication.run(ChatAppApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner createDefaultChatRoom() {
+        return args -> {
+            if (chatRoomRepository.findByName("Public").isEmpty()) {
+                ChatRoom publicRoom = ChatRoom.builder()
+                        .name("Public")
+                        .build();
+                chatRoomRepository.save(publicRoom);
+            }
+        };
     }
 
 }

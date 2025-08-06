d80711dfbe8d655438eab0efa2472cbdbf92aed0
AvargX <avargx@gmail.com>
2025-08-06 16:07:47 +0900

    feat: enable WebSocketJwtInterceptor in WebSocketConfig

--- src/main/java/ru/mypetproject/chatapp/config/WebSocketConfig.java
+++ src/main/java/ru/mypetproject/chatapp/config/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package ru.mypetproject.chatapp.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -9,10 +10,14 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Autowired
+    private WebSocketJwtInterceptor webSocketJwtInterceptor;
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
                 .setAllowedOrigins("*")
+                .addInterceptors(webSocketJwtInterceptor)
                 .withSockJS();
     }
 

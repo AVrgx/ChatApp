2b6b031506bd2507cfc071452943f38bc94f494e
AvargX <avargx@gmail.com>
2025-08-06 15:35:17 +0900

    feat: added ChatMessage dto

---
+++ src/main/java/ru/mypetproject/chatapp/dto/ChatMessage.java
@@ -0,0 +1,23 @@
+package ru.mypetproject.chatapp.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.awt.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatMessage {
+    private String content;
+    private String sender;
+    private String timestamp;
+    private MessageType type;
+
+    public enum MessageType {
+        CHAT, JOIN, LEAVE
+    }
+}
